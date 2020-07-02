0222c47c8630c8bb63a9fe0ef6988da218de4b28
VincentFSU <54596770+VincentFSU@users.noreply.github.com>
2020-07-02 16:03:20 -0400

    Added Scaling

--- train_model.py
+++ train_model.py
@@ -1,6 +1,7 @@
 import joblib
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
@@ -11,10 +12,19 @@ df = pd.read_csv("usedvehicles.csv")
 
 # Create the X and y arrays
 X = df[["year", "odometer"]]
-y = df["price"]
+y = df[["price"]]
+
+X_scaler = MinMaxScaler(feature_range=(0, 1))
+y_scaler = MinMaxScaler(feature_range=(0, 1))
+
+X[X.columns] = X_scaler.fit_transform(X[X.columns])
+y[y.columns] = y_scaler.fit_transform(y[y.columns])
+
+X = X.to_numpy()
+y = y.to_numpy()
 
 # Split the data set in a training set (75%) and a test set (25%)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
 # Create the Linear Regression model
 model = LinearRegression()
@@ -25,13 +35,16 @@ model.fit(X_train, y_train)
 # Save the trained model to a file so we can use it to make predictions later
 joblib.dump(model, 'used_car_value_model.pkl')
 
+joblib.dump(X_scaler, "X_scaler.pkl")
+joblib.dump(y_scaler, "y_scaler.pkl")
+
 # Report how well the model is performing
 print("Model training results:")
 
 # Report an error rate on the training set
-mse_train = mean_absolute_error(y_train, model.predict(X_train))
+mse_train = mean_absolute_error(y_scaler.inverse_transform(y_train), y_scaler.inverse_transform(model.predict(X_train)))
 print(f" - Training Set Error: {mse_train}")
 
 # Report an error rate on the test set
-mse_test = mean_absolute_error(y_test, model.predict(X_test))
+mse_test = mean_absolute_error(y_scaler.inverse_transform(y_test), y_scaler.inverse_transform(model.predict(X_test)))
 print(f" - Test Set Error: {mse_test}")
--- use_model.py
+++ use_model.py
@@ -4,6 +4,8 @@ from tkinter import *
 root = Tk()
 # Load our trained model
 model = joblib.load('used_car_value_model.pkl')
+X_scaler = joblib.load('X_scaler.pkl')
+y_scaler = joblib.load('y_scaler.pkl')
 
 year_in_label = Label(root, text='Vehicle Year:').grid(row=0)
 odometer_in_label = Label(root, text='Odometer(mi)').grid(row=1)
@@ -17,8 +19,10 @@ e_odometer.grid(row=1,column=1)
 def predict_click():
     car_1 = [int(e_year.get()), float(e_odometer.get())]
     cars = [car_1]
-    car_values = model.predict(cars)
-    predicted_value = car_values[0]
+    scaled_car_data = X_scaler.transform(cars)
+    car_values = model.predict(scaled_car_data)
+    unscaled_car_values = y_scaler.inverse_transform(car_values)
+    predicted_value = unscaled_car_values[0][0]
     details_label = Label(root, text="Car details:", anchor=W).grid(row=3)
     year_label = Label(root, text=f"vehicle year: {car_1[0]}").grid(row=4)
     odometer_label = Label(root, text=f"{car_1[1]} miles on odometer").grid(row=5)
