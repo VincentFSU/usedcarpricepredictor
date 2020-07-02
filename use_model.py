2d35b026e11b2048fee494a0202f4324b578a268
VincentFSU <54596770+VincentFSU@users.noreply.github.com>
2020-07-02 15:16:34 -0400

    Initial Commit

---
+++ use_model.py
@@ -0,0 +1,32 @@
+import joblib
+from tkinter import *
+
+root = Tk()
+# Load our trained model
+model = joblib.load('used_car_value_model.pkl')
+
+year_in_label = Label(root, text='Vehicle Year:').grid(row=0)
+odometer_in_label = Label(root, text='Odometer(mi)').grid(row=1)
+
+e_year = Entry(root)
+e_year.grid(row=0,column=1)
+e_odometer = Entry(root)
+e_odometer.grid(row=1,column=1)
+
+
+def predict_click():
+    car_1 = [int(e_year.get()), float(e_odometer.get())]
+    cars = [car_1]
+    car_values = model.predict(cars)
+    predicted_value = car_values[0]
+    details_label = Label(root, text="Car details:", anchor=W).grid(row=3)
+    year_label = Label(root, text=f"vehicle year: {car_1[0]}").grid(row=4)
+    odometer_label = Label(root, text=f"{car_1[1]} miles on odometer").grid(row=5)
+    price_label = Label(root, text=f"Estimated price: ${predicted_value:,.2f}").grid(row=6)
+    formatting_label = Label(root, text="").grid(row=7)
+
+
+predictButton = Button(root, bg="gray", text="Predict Price", padx=50, pady=10, command=predict_click).grid(row=2)
+
+
+root.mainloop()
